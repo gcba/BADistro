565d427e582a09678456986612351da3566c46a0
Germán <germangcba@gmail.com>
2015-10-06 16:58:25 -0300

    Agrega Carrousel

---
+++ modules/custom/badistro_bloques/templates/badistro_bloques_carrousel.tpl.php
@@ -0,0 +1,30 @@
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <?php foreach ($slides as $key => $slide) { ?>
+    <li data-target="#carousel-example-generic" data-slide-to="<?php echo $slide->id; ?>" class="<?php echo $key == 0 ? 'active' : ''; ?>"></li>
+    <?php } ?>
+  </ol>
+  <div class="carousel-inner" role="listbox">
+    <?php foreach ($slides as $key => $slide) { ?>
+    <!-- Wrapper for slides -->
+      <div class="item <?php echo $key == 0 ? 'active' : ''; ?>">
+        <?php echo $slide->imagen; ?>
+        <div class="carousel-caption">
+          <h3><?php echo $slide->titulo; ?></h3>
+          <p><?php echo $slide->caption; ?></p>
+        </div>
+      </div>
+    <?php } ?>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only"><?php echo t('Anterior'); ?></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only"><?php echo t('Siguiente'); ?></span>
+  </a>
+</div>
