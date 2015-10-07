85d8ac956caa17a8518899d35d7524c84ae17b26
Germán <germangcba@gmail.com>
2015-10-07 11:05:32 -0300

    Agrega bloque Shortcut

---
+++ modules/custom/badistro_bloques/templates/badistro_bloques_shortcut.tpl.php
@@ -0,0 +1,7 @@
+<a class="col-sm-6 col-md-3 shortcut" href="http://www.buenosaires.gob.ar/tramites">
+   <span class="bg-blue-lt" style="background-color: ">
+     <span style="background-image:url(http://www.buenosaires.gob.ar/sites/gcaba/files/iconos-v3/short-apoyo_virtual.png);background-repeat: no-repeat; background-position: center;"></span>
+   </span>
+   <h3>Lorem ipsum dolor sit amet</h3>
+   <p>Asunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</a>
