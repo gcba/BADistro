f400a1c34aec46a2cf778ae1c846cae89566158a
Agurtzane <agur.urrutia@gmail.com>
2015-10-07 14:29:36 -0300

    agrega template bloque listado de links

--- modules/custom/badistro_bloques/templates/badistro_bloques_listado_links.tpl.php
+++ modules/custom/badistro_bloques/templates/badistro_bloques_listado_links.tpl.php
@@ -1,18 +1,15 @@
 <?php ?>
-<ul>
-  <li>
-    <a href="#" title="">
-      Google
-    </a>
-  </li>
-  <li>
-    <a href="#" title="">
-      Google
-    </a>
-  </li>
-  <li>
-    <a href="#" title="">
-      Google
-    </a>
-  </li>
-</ul>
+<div class="separator clearfix"></div>
+
+            <ul class="list-group">
+                <li class="list-group-item"><a href="#"><h4>Cras justo odio</h4><p>Cras justo odio</p></a></li>
+                <li class="list-group-item"><a href="#"><h4>Dapibus ac facilisis in</h4><p>Cras justo odio</p></a></li>
+                <li class="list-group-item"><a href="#"><h4>Morbi leo risus</h4><p>Cras justo odio</p></a></li>
+                <li class="list-group-item"><a href="#"><h4>Porta ac consectetur ac</h4><p>Cras justo odio</p></a></li>
+                <li class="list-group-item"><a href="#"><h4>Vestibulum at eros</h4><p>Cras justo odio</p></a></li>
+            </ul>
+
+
+              
+                <button type="button" class="btn btn-default btn-lg btn-block">Ver todos trámites</button>
+          </div>
