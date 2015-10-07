7950848bbe0f1c077b5d63934fc12f036f42c9f7
Germán <germangcba@gmail.com>
2015-10-07 11:03:09 -0300

    Agrega bloque Listado de Links

---
+++ modules/custom/badistro_bloques/templates/badistro_bloques_listado_links.tpl.php
@@ -0,0 +1,18 @@
+<?php ?>
+<ul>
+  <li>
+    <a href="#" title="">
+      Google
+    </a>
+  </li>
+  <li>
+    <a href="#" title="">
+      Google
+    </a>
+  </li>
+  <li>
+    <a href="#" title="">
+      Google
+    </a>
+  </li>
+</ul>
