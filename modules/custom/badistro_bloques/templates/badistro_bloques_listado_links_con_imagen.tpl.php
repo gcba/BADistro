bb98d41b8b92dda893647f974b86b811c73c58d4
Agurtzane <agur.urrutia@gmail.com>
2015-10-09 16:02:23 -0300

    agrega maqueta listado de links con foto

--- modules/custom/badistro_bloques/templates/badistro_bloques_listado_links_con_imagen.tpl.php
+++ modules/custom/badistro_bloques/templates/badistro_bloques_listado_links_con_imagen.tpl.php
@@ -1,1 +1,80 @@
-YEAAAAAAAH
+            <h2 class="text-left">
+                Noticias
+              </h2>
+            
+
+
+
+
+
+
+
+            <!-- widget foto + texto -->
+                <div class="row-over"><a href="#">
+                <div class="media">
+                <div class="media-left media-middle">
+                <img class="media-object img-rounded" src="http://lorempixel.com/100/80/" alt="..."/>
+                
+                </div>
+                <div class="media-body">
+                <h4 class="media-heading">Lorem ipsum dolor sit amet </h4>
+                <p>Lorem ipsum dolor sit amet Integer molestie lorem at massa</p>
+                </div>
+                </div> 
+              </a>
+                </div> 
+            <!-- end widget foto + texto -->     
+   
+             <!-- widget foto + texto -->
+                <div class="row-over"><a href="#">
+                <div class="media">
+                <div class="media-left media-middle">
+                <img class="media-object img-rounded" src="http://lorempixel.com/100/80/" alt="..."/>
+                
+                </div>
+                <div class="media-body">
+                <h4 class="media-heading">Lorem ipsum dolor sit amet </h4>
+                <p>Lorem ipsum dolor sit amet Integer molestie lorem at massa</p>
+                </div>
+                </div> 
+              </a>
+                </div> 
+            <!-- end widget foto + texto -->    
+
+            <!-- widget foto + texto -->
+                <div class="row-over"><a href="#">
+                <div class="media">
+                <div class="media-left media-middle">
+                <img class="media-object img-rounded" src="http://lorempixel.com/100/80/" alt="..."/>
+                
+                </div>
+                <div class="media-body">
+                <h4 class="media-heading">Lorem ipsum dolor sit amet </h4>
+                <p>Lorem ipsum dolor sit amet Integer molestie lorem at massa</p>
+                </div>
+                </div> 
+                </a>
+                </div> 
+                
+            <!-- end widget foto + texto -->   
+
+                <div class="row-over"><a href="#">
+                <div class="media">
+                <div class="media-left media-middle">
+                <img class="media-object img-rounded" src="http://lorempixel.com/100/80/" alt="..."/>
+                
+                </div>
+                <div class="media-body">
+                <h4 class="media-heading">Lorem ipsum dolor sit amet </h4>
+                <p>Lorem ipsum dolor sit amet Integer molestie lorem at massa</p>
+                </div>
+                </div> 
+                </a>
+                </div> 
+                
+            <!-- end widget foto + texto --> 
+                  <div class="separator clearfix"></div>  
+                  <button type="button" class="btn btn-default btn-lg btn-block">Ver todas las Noticias</button>
+
+          
+           
