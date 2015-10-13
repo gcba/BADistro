36cdb4d6150ae0cee96c61c71ccf248439a112db
Germán <germangcba@gmail.com>
2015-10-13 16:02:37 -0300

    Agrega configuracion: referenciar pagina, noticia o tramite

--- modules/custom/badistro_bloques/templates/badistro_bloques_shortcut.tpl.php
+++ modules/custom/badistro_bloques/templates/badistro_bloques_shortcut.tpl.php
@@ -1,13 +1,5 @@
-<div class="col-md-3">
-  <div class="shortcut <?php echo $color; ?>">
-    <span class="glyphicon <?php echo $icono; ?>"></span>
-  </div>
-
-  <h3 class="text-center">
-  <?php echo $titulo; ?>
-  </h3>
-
-  <p class="text-center">
-  <?php echo $texto; ?>
-  </p>
+<div class="shortcut <?php echo $color; ?>">
+  <span class="glyphicon <?php echo $icono; ?>"></span>
 </div>
+<h3 class="text-center"><?php echo $titulo; ?></h3>
+<p class="text-center"><?php echo $texto; ?></p>
