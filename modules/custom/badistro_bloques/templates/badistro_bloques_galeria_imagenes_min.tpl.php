d8a75f529e91e3151875aaff971d206de2104f4c
Agurtzane <agur.urrutia@gmail.com>
2015-10-22 16:05:33 -0300

    agrega template galeria de imagenes miniatura

---
+++ modules/custom/badistro_bloques/templates/badistro_bloques_galeria_imagenes_min.tpl.php
@@ -0,0 +1,16 @@
+<div class="row">
+			<div class="col-xs-6 col-md-3">
+			<div class="thumbnail">
+			    
+			      <img src="http://lorempixel.com/300/250" data-src="holder.js/100%x180" alt="..." data-toggle="modal" data-target=".bs-example-modal-lg-1">
+			     
+			    	<div class="modal fade bs-example-modal-lg-1" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+					  <div class="modal-dialog modal-lg">
+					    <div class="modal-content">
+					      <img src="http://lorempixel.com/800/400" />
+					    </div>
+					  </div>
+					</div>
+			</div>
+			</div>
+</div>
