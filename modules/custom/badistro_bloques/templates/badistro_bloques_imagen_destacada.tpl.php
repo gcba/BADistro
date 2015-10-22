bf9e9373186b29d2e621649bf412f5b7433783c7
Agurtzane <agur.urrutia@gmail.com>
2015-10-22 16:00:56 -0300

    agrega template imagen destacada

--- modules/custom/badistro_bloques/templates/badistro_bloques_imagen_destacada.tpl.php
+++ modules/custom/badistro_bloques/templates/badistro_bloques_imagen_destacada.tpl.php
@@ -1,3 +1,4 @@
-<?php
-
-print 'hola';
+<div class="jumbotron jumbotron-main jumbotron-misc">
+       <h2 class="jumbo-idestacada-title">Jumbotron imagen destacada</h2>
+        <p class="jumbo-idestacada">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis, mi et sollicitudin tristique, ipsum risus placerat nunc, suscipit tincidunt magna metus vel lectus.</a></p>
+</div>
