0a6cff450f42013e210a88538660b9c02c8d7c99
Germán <germangcba@gmail.com>
2015-10-23 13:39:41 -0300

    Ajustes en Galeria de Imagenes

--- modules/custom/badistro_bloques/templates/badistro_bloques_galeria_imagenes_min.tpl.php
+++ modules/custom/badistro_bloques/templates/badistro_bloques_galeria_imagenes_min.tpl.php
@@ -1,16 +1,18 @@
-<div class="row">
+<?php foreach (array_chunk($imagenes, 4) as $key => $row) { ?>
+	<div class="row">	
+		<?php foreach ($row as $k => $item) { ?>
 			<div class="col-xs-6 col-md-3">
-			<div class="thumbnail">
-			    
-			      <img src="http://lorempixel.com/300/250" data-src="holder.js/100%x180" alt="..." data-toggle="modal" data-target=".bs-example-modal-lg-1">
-			     
-			    	<div class="modal fade bs-example-modal-lg-1" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+				<div class="thumbnail">
+		      <?php print $item['thumb']; ?>
+			    <div class="modal fade badistro-bloques-galeria-imagenes-<?php print $k; ?>" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
 					  <div class="modal-dialog modal-lg">
 					    <div class="modal-content">
-					      <img src="http://lorempixel.com/800/400" />
+					      <?php print $item['original']; ?>
 					    </div>
 					  </div>
 					</div>
+				</div>
 			</div>
-			</div>
-</div>
+		<?php } ?>
+	</div>
+<?php } ?>
