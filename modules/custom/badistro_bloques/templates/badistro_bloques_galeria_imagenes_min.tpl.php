ea17afd6f52dbe85910948d70721a23450c842c1
Germán <germangcba@gmail.com>
2015-10-26 16:47:38 -0300

    Corrige bug en panel Galeria de Imagenes

--- modules/custom/badistro_bloques/templates/badistro_bloques_galeria_imagenes_min.tpl.php
+++ modules/custom/badistro_bloques/templates/badistro_bloques_galeria_imagenes_min.tpl.php
@@ -2,16 +2,7 @@
 	<div class="row">	
 		<?php foreach ($row as $k => $item) { ?>
 			<div class="col-xs-6 col-md-3">
-				<div class="thumbnail">
-		      <?php print $item['thumb']; ?>
-			    <div class="modal fade badistro-bloques-galeria-imagenes-<?php print $k; ?>" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
-					  <div class="modal-dialog modal-lg">
-					    <div class="modal-content">
-					      <?php print $item['original']; ?>
-					    </div>
-					  </div>
-					</div>
-				</div>
+				<?php print $item['render']; ?>
 			</div>
 		<?php } ?>
 	</div>
---
+++ modules/custom/badistro_bloques/templates/badistro_bloques_galeria_imagenes_min_item.tpl.php
@@ -0,0 +1,10 @@
+<div class="thumbnail">
+  <?php print $thumb; ?>
+  <div class="modal fade badistro-bloques-galeria-imagenes-<?php print $key; ?>" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <?php print $original; ?>
+      </div>
+    </div>
+  </div>
+</div>
