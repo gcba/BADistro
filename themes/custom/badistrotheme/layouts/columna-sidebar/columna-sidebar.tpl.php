48afc3f75229a6d8176761818fdbeaea86e01774
Germán <germangcba@gmail.com>
2015-10-26 16:56:48 -0300

    Agrega contenedor del contenido

--- themes/custom/badistrotheme/layouts/columna-sidebar/columna-sidebar.tpl.php
+++ themes/custom/badistrotheme/layouts/columna-sidebar/columna-sidebar.tpl.php
@@ -2,22 +2,24 @@
 	<div class="panel-panel-full-width clearfix">
     <?php print $content['full']; ?>
   </div>
-	<!-- Top content -->
-	<div class="col-md-12">
-  	<?php print $content['top']; ?>
-  </div>
-	<!-- Sidebar content -->
-	<div class="col-md-4 col-sm-4 col-xs-12">
-		<!-- <nav> ? -->
-		<div><?php print $content['sidebar']; ?></div>
-	</div>
-	<!-- Main content -->
-	<div class="col-md-8 col-sm-8 col-xs-12">
-		<!-- <article> ? -->
-		<div><?php print $content['column']; ?></div>
-	</div>
-	<!-- Bottom content -->
-	<div class="clearfix">
-		<div><?php print $content['bottom']; ?></div>
+  <div class="container">
+		<!-- Top content -->
+		<div class="col-md-12">
+	  	<?php print $content['top']; ?>
+	  </div>
+		<!-- Sidebar content -->
+		<div class="col-md-4 col-sm-4 col-xs-12">
+			<!-- <nav> ? -->
+			<div><?php print $content['sidebar']; ?></div>
+		</div>
+		<!-- Main content -->
+		<div class="col-md-8 col-sm-8 col-xs-12">
+			<!-- <article> ? -->
+			<div><?php print $content['column']; ?></div>
+		</div>
+		<!-- Bottom content -->
+		<div class="clearfix">
+			<div><?php print $content['bottom']; ?></div>
+		</div>
 	</div>
 </div>
