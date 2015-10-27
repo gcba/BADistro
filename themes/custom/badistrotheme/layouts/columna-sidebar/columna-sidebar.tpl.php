50bc269164ae76425af7d552c1286dd72d385b4d
Germán <germangcba@gmail.com>
2015-10-27 12:18:08 -0300

    Mueve sidebar a la derecha

--- themes/custom/badistrotheme/layouts/columna-sidebar/columna-sidebar.tpl.php
+++ themes/custom/badistrotheme/layouts/columna-sidebar/columna-sidebar.tpl.php
@@ -7,15 +7,13 @@
 		<div class="col-md-12">
 	  	<?php print $content['top']; ?>
 	  </div>
-		<!-- Sidebar content -->
-		<div class="col-md-4 col-sm-4 col-xs-12">
-			<!-- <nav> ? -->
-			<div><?php print $content['sidebar']; ?></div>
-		</div>
 		<!-- Main content -->
 		<div class="col-md-8 col-sm-8 col-xs-12">
-			<!-- <article> ? -->
 			<div><?php print $content['column']; ?></div>
+		</div>
+		<!-- Sidebar content -->
+		<div class="col-md-4 col-sm-4 col-xs-12">
+			<div><?php print $content['sidebar']; ?></div>
 		</div>
 		<!-- Bottom content -->
 		<div class="clearfix">
