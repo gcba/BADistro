8b692bd5758ed47b8a1fd7cd75fcbcefbf73cabd
Agurtzane <agur.urrutia@gmail.com>
2015-11-03 16:02:54 -0300

    agrega diseno share redes sociales

--- modules/custom/badistro_bloques/templates/badistro_bloques_redes_sociales_compartir.tpl.php
+++ modules/custom/badistro_bloques/templates/badistro_bloques_redes_sociales_compartir.tpl.php
@@ -1,3 +1,7 @@
 <h2 class="text-left"><?php echo $titulo; ?></h2>
-<div class="fb-share-button" data-href="" data-layout="button"></div>
+<div class="redes-wrapper">
+<div class="fb-share-button " data-href="" data-layout="button"></div>
+
 <a href="https://twitter.com/share" class="twitter-share-button" data-count="none">Tweet</a>
+
+</div>
