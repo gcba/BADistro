7d88d5c44d971289fa6cd26ee34dae160d01ab5a
Agurtzane <agur.urrutia@gmail.com>
2015-11-05 14:48:17 -0300

    agrega shared redes en page agrega estilo backend

--- modules/custom/badistro_bloques/templates/badistro_bloques_redes_sociales_compartir.tpl.php
+++ modules/custom/badistro_bloques/templates/badistro_bloques_redes_sociales_compartir.tpl.php
@@ -1,5 +1,6 @@
-<h2 class="text-left"><?php echo $titulo; ?></h2>
 <div class="redes-wrapper">
+<p class="label-destacado"><?php echo $titulo; ?></p>
+
 <div class="fb-share-button " data-href="" data-layout="button"></div>
 
 <a href="https://twitter.com/share" class="twitter-share-button" data-count="none">Tweet</a>
