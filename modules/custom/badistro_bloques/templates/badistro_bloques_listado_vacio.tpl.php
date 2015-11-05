1ba60b94b1afbb412e77c720bb96fb4ce420f468
Germán <germangcba@gmail.com>
2015-11-05 15:49:14 -0300

    Agrega template para listado sin items

---
+++ modules/custom/badistro_bloques/templates/badistro_bloques_listado_vacio.tpl.php
@@ -0,0 +1,4 @@
+<h2 class="text-left"><?php echo $titulo; ?></h2>
+<div class="badistro-bloques-listado-vacio alert alert-default">
+  <?php print t('No hay items para mostrar. ' .  l('Carga contenido', 'node/add') .  ' y van a ir apareciendo.'); ?>
+</div>
