6d0e30d5c76665c115d995ca752e56cf4872156c
Germán <germangcba@gmail.com>
2015-11-09 15:19:39 -0300

    Ajuste

--- modules/custom/badistro_bloques/templates/badistro_bloques_shortcut.tpl.php
+++ modules/custom/badistro_bloques/templates/badistro_bloques_shortcut.tpl.php
@@ -1,5 +1,7 @@
-<div class="shortcut <?php echo $color; ?>">
-  <span class="glyphicon <?php echo $icono; ?>"></span>
+<div class="clearfix <?php echo $css; ?>">
+  <div class="shortcut <?php echo $color; ?>">
+    <span class="glyphicon <?php echo $icono; ?>"></span>
+  </div>
+  <h3 class="text-center"><?php echo $titulo; ?></h3>
+  <p class="text-center"><?php echo $texto; ?></p>
 </div>
-<h3 class="text-center"><?php echo $titulo; ?></h3>
-<p class="text-center"><?php echo $texto; ?></p>
