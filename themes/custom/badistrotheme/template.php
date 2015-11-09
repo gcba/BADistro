1a4eb2b0dc9edee7898a4e669d11b3f1d1b913eb
Germán <germangcba@gmail.com>
2015-11-09 15:19:11 -0300

    Ajuste de clases de los items de menus

--- themes/custom/badistrotheme/template.php
+++ themes/custom/badistrotheme/template.php
@@ -6,6 +6,7 @@ function badistrotheme_menu_link(array $variables) {
   $variables['element']['#attributes']['id'][] = $variables['element']['#original_link']['menu_name'] . '-' . $variables['element']['#original_link']['mlid'];
   $variables['element']['#attributes']['class'][] = 'menu-' . $variables['element']['#original_link']['mlid'];
   $variables['element']['#localized_options']['attributes']['class'][] = 'menu-' . $variables['element']['#original_link']['mlid'];
+  $variables['element']['#localized_options']['attributes']['class'][] = $variables['element']['#original_link']['menu_name'] . '-' . check_plain($variables['element']['#original_link']['title']);
   return theme_menu_link($variables);
 }
 ?>
